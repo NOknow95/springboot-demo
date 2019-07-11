7ebb553fb0efbdbce950d7483e7ccc288cc45670
wangjianwen <3027001339@iwhalecloud.com>
2019-07-11 20:41:54 +0800

    first commit

---
+++ src/main/java/com/jw/springboodemo/SpringbooDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jw.springboodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooDemoApplication.class, args);
+    }
+
+}
